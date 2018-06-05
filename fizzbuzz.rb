5269daf19aad77ebcf7f69257987231caa021933
alexencinas <shoogiee123@gmail.com>
2018-06-05 19:28:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,12 @@ def fizzbuzz
   number = gets.chomp.to_i
   if number %3==0 && number %5==0
     puts "Fizzbuzz"
+  elsif number %3==0
+    puts "Fizz"
+    elsif number %5==0
+    puts "buzz"
+  else 
+  end 
 end
 fizzbuzz
    
