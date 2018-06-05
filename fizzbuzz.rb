b971d09618be23ed7603f00ec043be0447d6ae45
alexencinas <shoogiee123@gmail.com>
2018-06-05 19:21:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,14 @@
-def fizzbuzz (number)
-  if number /3
+def fizzbuzz
+  number = gets.chomp
+  if number % 3
     puts "Fizz"
-  elsif number / 5
+  elsif number % 5
     puts ""
   puts "Buzz"
+  elsif number % 3 && number %5
+  puts "Fizzbuzz"
+else
+  puts "nil"
+end
+fizzbuzz
    
